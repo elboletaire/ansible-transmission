3e394e1884f8b938227a547aa482e494d1afa4e5
Òscar Casajuana <elboletaire@underave.net>
2015-10-13 00:51:35 +0200

    Removed idempotence tests as it won't pass ever

--- tests/test.sh
+++ tests/test.sh
@@ -13,9 +13,3 @@ ansible-playbook -i $INVENTORY_FILE -c local --syntax-check -vv $PLAYBOOK
 
 # Check role
 ansible-playbook -i $INVENTORY_FILE -c local --sudo -vv $PLAYBOOK
-
-# Check indempotence
-ansible-playbook -i $INVENTORY_FILE -c local --sudo -vv $PLAYBOOK \
-| grep -q 'changed=0.*failed=0' \
-&& (echo 'Idempotence test: pass' && exit 0) \
-|| (echo 'Idempotence test: fail' && exit 1)
